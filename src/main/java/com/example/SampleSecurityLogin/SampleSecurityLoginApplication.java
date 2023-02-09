24f65a6c4be64949e3ba5ec16e42175bf5a04d67
lsung <cp.lsung@gmail.com>
2023-02-10 02:38:58 +0900

    샘플 시큐리티 로그인 구현용 신규작성

---
+++ src/main/java/com/example/SampleSecurityLogin/SampleSecurityLoginApplication.java
@@ -0,0 +1,13 @@
+package com.example.SampleSecurityLogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSecurityLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSecurityLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SampleSecurityLogin/SampleSecurityLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SampleSecurityLogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSecurityLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
