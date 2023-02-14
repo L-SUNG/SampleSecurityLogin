b53b5c73bf0f728073aea4fb43262d2422359c49
lsung <cp.lsung@gmail.com>
2023-02-14 10:35:16 +0900

    로그인 후 세션을 확인하여 로그인 멤버ID 표시 및 로그아웃 버튼 전환

--- src/main/java/com/example/SampleSecurityLogin/web/PageController.java
+++ src/main/java/com/example/SampleSecurityLogin/web/PageController.java
@@ -1,6 +1,15 @@
 package com.example.SampleSecurityLogin.web;
 
+import com.example.SampleSecurityLogin.domain.member.Member;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -9,7 +18,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class PageController {
 
     @GetMapping("/")
-    public String index() {
+    public String index(HttpSession session) {
+
+        // 스프링 시큐리티 컨텍스트 취득
+        Object securityContextObject = session.getAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY);
+        if(securityContextObject != null) {
+            SecurityContext securityContext = (SecurityContext)securityContextObject;
+            Authentication authentication = securityContext.getAuthentication();
+            // 인증정보로 부터 멤버 ID를 취득하여 세션에 설정
+            session.setAttribute("member", authentication.getName());
+        }
+
         return "index";
     }
 
